98f03b8bcf576d312fcf96a0caa7cdfb52a04ab4
Gabriel Menezes <40703319+gabrielsmenezes@users.noreply.github.com>
2021-03-12 21:26:34 -0400

    Adicionando os novos pesos

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         PontuacaoMZ
 // @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  Ferramenta que dá uma pontuação para seus jogadores para cada posição
+// @version      0.3
+// @description  Ferramenta que da uma pontuação para seus jogadores para cada posição
 // @author       Gabriel Menezes (mz_seds)
 // @match        https://www.managerzone.com/*
 // @grant        none
@@ -16,99 +16,142 @@
         var players_html = players_container.children;
 
 
-
     var pesos =
         {
             "Goleiro": {
-                "Velocidade":0.09,
-                "Resistência":0.09,
-                "Inteligência":0.09,
-                "Passe Curto":0.02,
-                "Chute":0,
-                "Cabeceio":0,
-                "Defesa a Gol":0.55,
-                "Controle de Bola":0.09,
-                "Desarme":0,
-                "Passe Longo":0.02,
-                "Bola Parada":0,
-                "Experiência":0.05,
-                "Forma":0
+                "Velocidade": 2,
+                "Resistência": 8,
+                "Inteligência": 8,
+                "Passe Curto": 0,
+                "Chute": 0,
+                "Cabeceio": 0,
+                "Defesa a Gol": 8,
+                "Controle de Bola": 2,
+                "Desarme": 0,
+                "Passe Longo": 2,
+                "Bola Parada": 0,
+                "Experiência": 8,
+                "Forma": 8
             },
             "Zagueiro": {
-                "Velocidade":0.25,
-                "Resistência":0.16,
-                "Inteligência":0.07,
-                "Passe Curto":0.02,
-                "Chute":0,
-                "Cabeceio":0,
-                "Defesa a Gol":0.0,
-                "Controle de Bola":0.08,
-                "Desarme":0.3,
-                "Passe Longo":0.07,
-                "Bola Parada":0,
-                "Experiência":0.05,
-                "Forma":0
+                "Velocidade": 7,
+                "Resistência": 3,
+                "Inteligência": 8,
+                "Passe Curto": 6,
+                "Chute": 0,
+                "Cabeceio": 0,
+                "Defesa a Gol": 0,
+                "Controle de Bola": 3,
+                "Desarme": 8,
+                "Passe Longo": 3,
+                "Bola Parada": 0,
+                "Experiência": 8,
+                "Forma": 3
             },
-            "Ancora": {
-                "Velocidade":0.25,
-                "Resistência":0.18,
-                "Inteligência":0.05,
-                "Passe Curto":0.05,
-                "Chute":0,
-                "Cabeceio":0.02,
-                "Defesa a Gol":0.0,
-                "Controle de Bola":0.1,
-                "Desarme":0.25,
-                "Passe Longo":0.05,
-                "Bola Parada":0,
-                "Experiência":0.05,
-                "Forma":0
+            "Lateral": {
+                "Velocidade": 9,
+                "Resistência": 8,
+                "Inteligência": 3,
+                "Passe Curto": 7,
+                "Chute": 0,
+                "Cabeceio": 0,
+                "Defesa a Gol": 0,
+                "Controle de Bola": 7,
+                "Desarme": 9,
+                "Passe Longo": 4,
+                "Bola Parada": 0,
+                "Experiência": 10,
+                "Forma": 8
             },
-            "Meio": {
-                "Velocidade":0.15,
-                "Resistência":0.15,
-                "Inteligência":0.1,
-                "Passe Curto":0.15,
-                "Chute":0,
-                "Cabeceio":0,
-                "Defesa a Gol":0,
-                "Controle de Bola":0.12,
-                "Desarme":0.2,
-                "Passe Longo":0.08,
-                "Bola Parada":0,
-                "Experiência":0.05,
-                "Forma":0
+            "Volante": {
+                "Velocidade": 7,
+                "Resistência": 8,
+                "Inteligência": 8,
+                "Passe Curto": 8,
+                "Chute": 0,
+                "Cabeceio": 0,
+                "Defesa a Gol": 0,
+                "Controle de Bola": 2,
+                "Desarme": 8,
+                "Passe Longo": 7,
+                "Bola Parada": 0,
+                "Experiência": 8,
+                "Forma": 8
+            },
+            "Meia Central": {
+                "Velocidade": 7,
+                "Resistência": 8,
+                "Inteligência": 8,
+                "Passe Curto": 8,
+                "Chute": 1,
+                "Cabeceio": 0,
+                "Defesa a Gol":	0,
+                "Controle de Bola": 5,
+                "Desarme": 1,
+                "Passe Longo": 8,
+                "Bola Parada": 0,
+                "Experiência": 8,
+                "Forma": 8
+            },
+            "Meia Atacante": {
+                "Velocidade": 6,
+                "Resistência": 8,
+                "Inteligência": 8,
+                "Passe Curto": 8,
+                "Chute": 8,
+                "Cabeceio": 0,
+                "Defesa a Gol": 0,
+                "Controle de Bola": 7,
+                "Desarme": 7,
+                "Passe Longo": 7,
+                "Bola Parada": 0,
+                "Experiência": 8,
+                "Forma": 8
             },
             "Ponta": {
-                "Velocidade":0.25,
-                "Resistência":0.2,
-                "Inteligência":0.06,
-                "Passe Curto":0.04,
-                "Chute":0.05,
-                "Cabeceio":0,
-                "Defesa a Gol":0,
-                "Controle de Bola":0.15,
-                "Desarme":0.05,
-                "Passe Longo":0.15,
-                "Bola Parada":0,
-                "Experiência":0.05,
-                "Forma":0
-            }
-            ,
+               "Velocidade": 9,
+                "Resistência": 8,
+                "Inteligência": 5,
+                "Passe Curto": 3,
+                "Chute": 0,
+                "Cabeceio": 0,
+                "Defesa a Gol": 0,
+                "Controle de Bola": 8,
+                "Desarme": 0,
+                "Passe Longo": 8,
+                "Bola Parada": 0,
+                "Experiência": 8,
+                "Forma":	8
+            },
             "Atacante": {
-                "Velocidade":0.2,
-                "Resistência":0.15,
-                "Inteligência":0.05,
-                "Passe Curto":0.04,
-                "Chute":0.25,
-                "Cabeceio":0.11,
-                "Defesa a Gol":0,
-                "Controle de Bola":0.15,
-                "Desarme":0,
-                "Passe Longo":0,
-                "Bola Parada":0,
-                "Experiência":0.05,
-                "Forma":0
+                "Velocidade": 8,
+                "Resistência": 8,
+                "Inteligência": 8,
+                "Passe Curto": 3,
+                "Chute": 8,
+                "Cabeceio": 2,
+                "Defesa a Gol": 0,
+                "Controle de Bola": 6,
+                "Desarme": 1,
+                "Passe Longo": 0,
+                "Bola Parada": 0,
+                "Experiência": 8,
+                "Forma":	8
+            },
+            "Cabeçudo": {
+                "Velocidade": 4,
+                "Resistência": 8,
+                "Inteligência": 8,
+                "Passe Curto": 2,
+                "Chute": 8,
+                "Cabeceio": 8,
+                "Defesa a Gol": 0,
+                "Controle de Bola": 5,
+                "Desarme": 0,
+                "Passe Longo": 0,
+                "Bola Parada": 0,
+                "Experiência": 8,
+                "Forma": 8
             }
     }
 
@@ -135,6 +178,7 @@
             somatorioDaHabilidade = somatorioDaHabilidade + (jogador[habilidade] * pesos[posicao][habilidade])
             somatorioDoPeso = somatorioDoPeso + pesos[posicao][habilidade]
         })
+
 
         return somatorioDaHabilidade / somatorioDoPeso
     }
@@ -164,21 +208,27 @@
         var jogador = buscaHabilidadesDoJogador(allskillval)
 
 
-
         var goleiro = calcularNotaPorPosicao(jogador, "Goleiro")
         var zagueiro = calcularNotaPorPosicao(jogador, "Zagueiro")
-        var ancora = calcularNotaPorPosicao(jogador, "Ancora")
-        var meio = calcularNotaPorPosicao(jogador, "Meio")
+        var lateral = calcularNotaPorPosicao(jogador, "Lateral")
+        var volante = calcularNotaPorPosicao(jogador, "Volante")
+        var meia_central = calcularNotaPorPosicao(jogador, "Meia Central")
+        var meia_atacante = calcularNotaPorPosicao(jogador, "Meia Atacante")
         var ponta = calcularNotaPorPosicao(jogador, "Ponta")
         var atacante = calcularNotaPorPosicao(jogador, "Atacante")
+        var cabecudo = calcularNotaPorPosicao(jogador, "Cabeçudo")
 
         const posicoes = [
             {nome: "Goleiro", nota: goleiro},
             {nome:"Zagueiro", nota: zagueiro},
-            {nome:"Ancora", nota: ancora},
-            {nome:"Meio", nota: meio},
-            {nome:"Ponta", nota: ponta},
-            {nome:"Atacante", nota:atacante }
+            {nome:"Lateral", nota: lateral},
+            {nome:"Volante", nota: volante},
+            {nome:"Meia Central", nota: meia_central},
+            {nome:"Meia Atacante", nota:meia_atacante },
+            {nome:"Ponta", nota:ponta },
+            {nome:"Atacante", nota:atacante },
+            {nome:"Cabeçudo", nota:cabecudo },
+
         ]
 
         posicoes.sort((a,b) => (a.nota > b.nota) ? -1: 1)
